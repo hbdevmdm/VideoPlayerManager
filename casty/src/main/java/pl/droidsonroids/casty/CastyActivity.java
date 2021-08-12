da13499a571e7d84c2c95b745883bcf220e0f565
Krupal <krupal.mehta@hiddenbrains.in>
2021-08-12 15:56:21 +0530

    add casty lib

---
+++ casty/src/main/java/pl/droidsonroids/casty/CastyActivity.java
@@ -0,0 +1,34 @@
+package pl.droidsonroids.casty;
+
+import android.os.Bundle;
+
+import android.view.Menu;
+
+import androidx.annotation.CallSuper;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Extensible {@link AppCompatActivity}, which helps with setting widgets
+ */
+public abstract class CastyActivity extends AppCompatActivity {
+    protected Casty casty;
+
+    @CallSuper
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        casty = Casty.create(this);
+    }
+
+    @CallSuper
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        if (findViewById(R.id.casty_mini_controller) == null) {
+            casty.addMiniController();
+        }
+        casty.addMediaRouteMenuItem(menu);
+        return true;
+    }
+}
